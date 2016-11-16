4e54b9aef977fe301d62944616c70e2844ee6f0d
whitekkk <white_itsme@hotmail.com>
2016-11-16 12:30:47 +0700

    first

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  res.send('Hello')
 })
